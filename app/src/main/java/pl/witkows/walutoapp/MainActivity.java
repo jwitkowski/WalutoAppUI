985dfe0db1c42c4d008c92718b364fc5822cbe87
Witkowski <dev.witkowski@gmail.com>
2020-09-12 14:07:28 +0200

    poc

--- app/src/main/java/pl/witkows/walutoapp/MainActivity.java
+++ app/src/main/java/pl/witkows/walutoapp/MainActivity.java
@@ -3,6 +3,7 @@ package pl.witkows.walutoapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     CurrencyApi currencyApi = new CurrencyApi();
     private TextView currencyText;
     private Button currencyButton;
+    private EditText currencyValue;
+    private EditText currencyName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         currencyText = findViewById(R.id.currency_text);
         currencyButton = findViewById(R.id.get_currency_button);
+        currencyName = findViewById(R.id.currency_name);
+        currencyValue = findViewById(R.id.currency_value);
 
         currencyButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,11 +41,15 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            final CurrencyInfo result = currencyApi.getInfoForCurrency(Currency.CAD);
+                            String currencyValue = MainActivity.this.currencyValue.getText().toString();
+                            String currencyNameString = currencyName.getText().toString();
+                            Currency currency = Currency.getCurrency(currencyNameString);
+                            final int value = Integer.parseInt(currencyValue);
+                            final CurrencyInfo result = currencyApi.getInfoForCurrency(currency);
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    currencyText.setText(result.getName() + " " + result.getBuy());
+                                    currencyText.setText(result.getName() + " " + (result.getBuy() * value));
                                 }
                             });
                         } catch (IOException e) {
